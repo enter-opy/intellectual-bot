abaec9258579c63039fce2b02530be8a0e208240
enter-opy <vivekvijayan6282@gmail.com>
2022-02-01 23:34:07 +0530

    type effect

--- script.js
+++ script.js
@@ -16,21 +16,42 @@ function send() {
         messages.appendChild(usertext);
 
         let timeuser = document.createElement("li");
-        timeuser.className = "time"
+        timeuser.className = "timeuser"
         timeuser.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
         usertext.appendChild(timeuser);
 
         //
-        let bottext = document.createElement("li");
-        bottext.className = "bottext";
+        
+        let indicator = document.createElement("li");
+        indicator.className = "indicator";
+        indicator.id = "indicator";
 
-        bottext.innerHTML = value;
-        messages.appendChild(bottext);
+        let dot1 = document.createElement("span");
+        dot1.className = "dot-1";
+        let dot2 = document.createElement("span");
+        dot2.className = "dot-2";
+        let dot3 = document.createElement("span");
+        dot3.className = "dot-3";
 
-        let timebot = document.createElement("li");
-        timebot.className = "time"
-        timebot.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
-        bottext.appendChild(timebot);
-        //
+        messages.appendChild(indicator);
+        indicator.appendChild(dot1);
+        indicator.appendChild(dot2);
+        indicator.appendChild(dot3);
+
+        setTimeout(function () {
+            let bottext = document.createElement("li");
+            bottext.className = "bottext";
+            bottext.innerHTML = value;
+            messages.appendChild(bottext);
+        
+            let timebot = document.createElement("li");
+            timebot.className = "timebot"
+            timebot.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
+            bottext.appendChild(timebot);
+            messages.removeChild(indicator);
+        }, 500);
+        
+        
     }
 }
+
